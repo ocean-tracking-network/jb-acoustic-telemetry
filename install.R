efb1ac02afc8ecf176422d86587bced9a7e4bd26
JackVanish <bruce.delo@gmail.com>
2019-11-28 14:30:11 -0400

    Incremental changes to build.

--- install.R
+++ install.R
@@ -2,3 +2,6 @@ install.packages("devtools")
 
 # Tidyverse (data cleaning and arrangement)
 install.packages('ggplot2')
+
+install.packages("httr", dependencies = TRUE)
+install.packages("ncdf4",dependencies = TRUE)
