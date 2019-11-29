1b36b897a9f85d009441f199f095ab1e7b95a2fe
JackVanish <bruce.delo@gmail.com>
2019-11-29 15:11:03 -0400

    Corrected rgdal to r-rgdal

--- install.R
+++ install.R
@@ -1,7 +1,39 @@
 install.packages("devtools")
 
+# Tidyverse (data cleaning and arrangement)
+install.packages('ggplot2')
+install.packages('ggmap')
+install.packages('dplyr')
+install.packages('gganimate')
+
+# animated GIF encoder
+install.packages("gifski")
+
+# Concise way to manipulate datasets
+install.packages("data.table")
+
+# Geospatial Data Manipulation and Vis Packages
+install.packages('geosphere')
+#install.packages('rgdal')
+install.packages('raster')
+install.packages('mapdata')
+install.packages('maptools')
+install.packages("sf")
+install.packages("rnaturalearth")
+install.packages("plotly")
+install.packages("sp", dependencies = TRUE)
+
+# Network analysis
+install.packages('igraph')
+
+install.packages('viridis')
+install.packages('lunar')
+
+install.packages("httr", dependencies = TRUE)
+#install.packages("ncdf4",dependencies = TRUE)
+
 # XTractomatic - match locations in time and space w/ model data via ERDDAP
-#devtools::install_github("rmendels/xtractomatic")
+devtools::install_github("rmendels/xtractomatic")
 
 # VTrack and ATT - Tools for Telemetry Analysis
 devtools::install_github("rossdwyer/VTrack")
