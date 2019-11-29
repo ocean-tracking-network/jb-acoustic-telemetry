562f2109f8b26e1355cbc0d63d90a7c6d20fd5ca
JackVanish <bruce.delo@gmail.com>
2019-11-29 10:10:02 -0400

    Filled out Install again.

--- install.R
+++ install.R
@@ -31,3 +31,19 @@ install.packages('lunar')
 
 install.packages("httr", dependencies = TRUE)
 install.packages("ncdf4",dependencies = TRUE)
+
+# XTractomatic - match locations in time and space w/ model data via ERDDAP
+devtools::install_github("rmendels/xtractomatic")
+
+# VTrack and ATT - Tools for Telemetry Analysis
+devtools::install_github("rossdwyer/VTrack")
+devtools::install_github("vinayudyawer/ATT")
+
+# GLATOS - acoustic telemetry package that does filtering, vis, array simulation, etc.
+install.packages('remotes')
+library(remotes)
+install_url("https://gitlab.oceantrack.org/GreatLakes/glatos/repository/master/archive.zip",
+            build_opts = c("--no-resave-data", "--no-manual"))
+
+# Sensible Working Directory Manager
+install.packages("here")
